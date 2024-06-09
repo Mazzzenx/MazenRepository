95e04e5a25ff7e41b21ee9ab7cfede7e1f2d9712
Mazen Elhalawany <mazen17mazen12@yahoo.com>
2024-06-09 19:43:08 +0300

    lastest changes

---
+++ src/utils/middleware/cloudinary.upload.js
@@ -0,0 +1,36 @@
+import {cloudinaryConfig} from "../../config/cloud"
+
+const uploadImageToCloudinary = async (file, publicId, path) => {
+    try {
+        console.log(cloudinaryConfig);
+        const result = await  cloudinaryConfig.v2.uploader.upload(file, {
+            folder: path,
+            public_id: publicId,
+            overwrite: true,
+        },
+            function (error, result) {
+                if (error) {
+                    return {
+                        success: false,
+                        statusCode: 500,
+                        message: "something went wrong, could not upload media !"
+                    }
+                };
+            });
+        return {
+            success: true,
+            statusCode: 201,
+            message: "success",
+            data: result.url
+        }
+    }
+    catch (err) {
+        return {
+            success: false,
+            statusCode: 500,
+            message: err.message
+        }
+    }
+};
+
+export const uploadToCloudinar = uploadImageToCloudinary
