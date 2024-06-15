5f360f2be93968ed631b88a703eee61368cf8964
Mazen Elhalawany <121464762+Mazzzenx@users.noreply.github.com>
2024-06-15 03:43:48 +0300

    Update wishList.controller.js

--- src/modules/wishList/wishList.controller.js
+++ src/modules/wishList/wishList.controller.js
@@ -44,7 +44,7 @@ const removeFromWishList =
       $pull: { wishList: product },
     },
     { new: true }
-  );
+  ).populate("wishList");
   !results && next(new AppError("not found Review", 404));
   results && res.json({ message: "Done", results });
   }
@@ -58,7 +58,7 @@ const getAllWishList =
 
   let results = await userModel.findOne({ _id: req.user._id })
   !results && next(new AppError("not found Review", 404));
-  results && res.json({ message: "Done", results: results});
+  results && res.json({ message: "Done", results: results.wishList});
   }
 
   );
