3c8c9cddb3c8c50bad985a3abde6e2209f6dcf8a
Mazen Elhalawany <121464762+Mazzzenx@users.noreply.github.com>
2024-06-15 03:29:44 +0300

    Update wishList.controller.js

--- src/modules/wishList/wishList.controller.js
+++ src/modules/wishList/wishList.controller.js
@@ -58,7 +58,7 @@ const getAllWishList =
 
   let results = await userModel.findOne({ _id: req.user._id })
   !results && next(new AppError("not found Review", 404));
-  results && res.json({ message: "Done", results: results.wishList });
+  results && res.json({ message: "Done", results: results});
   }
 
   );
