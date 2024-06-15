b7f37ceaff15609363b9ebe68b19a73696175b90
Mazen Elhalawany <121464762+Mazzzenx@users.noreply.github.com>
2024-06-15 04:09:05 +0300

    Update wishList.controller.js

--- src/modules/wishList/wishList.controller.js
+++ src/modules/wishList/wishList.controller.js
@@ -55,18 +55,10 @@ const getAllWishList =
   catchAsyncError(
   
   async (req, res, next) => {
-  // let { product } = req.body;
 
   let results = await userModel.findOne({ _id: req.user._id })
-  //   let results = await userModel.findOneAndUpdate(
-  //   req.user._id,
-  //   {
-  //     $addToSet: { wishList: product },
-  //   },
-  //   { new: true }
-  // );
-  !results && next(new AppError("not found Review", 404));
-  results && res.json({ message: "Done", results: results.wishList});
+   !results && next(new AppError("not found Review", 404));
+  results && res.json({ message: "Done", results });
   }
 
   );
